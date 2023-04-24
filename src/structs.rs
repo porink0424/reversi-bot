fe80b6864d406334124d45791591698f803967a4
Daichi Kato <daichi5967@docomo.ne.jp>
2023-04-25 00:51:29 +0900

    Make it possible for Board to construct with new Board()

--- src/structs.rs
+++ src/structs.rs
@@ -10,3 +10,29 @@ pub struct Board {
     pub put_stones_count: usize,
     pub current_color: COLOR,
 }
+
+#[wasm_bindgen]
+impl Board {
+    #[wasm_bindgen(constructor)]
+    pub fn new() -> Board {
+        Board {
+            black_stones: 0,
+            white_stones: 0,
+            put_stones_count: 0,
+            current_color: COLOR::BLACK,
+        }
+    }
+
+    pub fn set(
+        &mut self,
+        black_stones: u64,
+        white_stones: u64,
+        put_stones_count: usize,
+        current_color: COLOR,
+    ) {
+        self.black_stones = black_stones;
+        self.white_stones = white_stones;
+        self.put_stones_count = put_stones_count;
+        self.current_color = current_color;
+    }
+}
